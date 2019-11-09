2ec6d590b8f016a4bf12a95c0f434b2a75a13ba1
OyinloluB <oyinloluwab@gmail.com>
2019-11-09 17:44:45 +0100

    Add

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: '/img/first.jpg',
+    image2: '/img/second.jpg',
+    angle: Math.PI / 9,
+    displacementImage: '/img/heightMap.png'
+});
